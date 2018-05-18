8e04852a0b08c286da08162c95e99fdbb104ac7c
Anh Pham Hoang <anhph@atmarkcafe.org>
2018-05-18 09:48:15 +0700

    Add delete and patch method

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 //local
 const {mongoose} = require('./db/mongoose');
@@ -48,6 +49,52 @@ app.get('/todos/:id', (req, res) => {
             res.status(400).send(e);
         });
     }
+
+});
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send('Page not found');
+    } else {
+        Todo.findByIdAndRemove(id).then((todo) => {
+            if (!todo) {
+                res.status(404).send('Page not found');
+            } else {
+                res.send(todo);
+            }
+        }).catch((e) => {
+            res.status(400).send(e);
+        });
+    }
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completedAt = null;
+        body.completed = false;
+    }
+
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            res.status(404).send('Page not found');
+        } else {
+            res.send(todo);
+        }
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
 
 });
 
