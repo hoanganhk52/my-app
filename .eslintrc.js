ea0713cac5cc409106f0b52b2d3321c1e78eefaa
acv-anhph <anhph@atmarkcafe.org>
2018-05-30 14:01:42 +0700

    replace npm by yarn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,34 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-	    "mocha": true
-    },
+	"env": {
+		"commonjs": true,
+		"es6": true,
+		"mocha": true,
+		"jquery": true
+	},
 	"plugins": [
 		"mocha"
 	],
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-	    "mocha/no-exclusive-tests": "error"
-    }
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"mocha/no-exclusive-tests": "error"
+	}
 };
